24757ee36bf25884250db454bf1c20d90faa534b
António Santos <antoniojps@ua.pt>
2018-08-07 21:37:17 +0100

    add BaseAuth component

---
+++ src/components/BaseAuth.vue
@@ -0,0 +1,46 @@
+<template>
+  <div v-if="hasPermissions">
+    <slot/>
+  </div>
+</template>
+
+<script>
+import { mapGetters } from 'vuex'
+
+export default {
+  props: {
+    requires: {
+      type: String,
+      default: 'user',
+    },
+    redirect: {
+      type: String,
+      required: false,
+      default: '',
+    },
+  },
+  computed: {
+    ...mapGetters('user', ['user', 'userLogedIn']),
+    hasAdminPermissions: function () {
+      if (this.user) return !!this.user.admin
+      return false
+    },
+    hasModeratorPermissions: function () {
+      if (this.user && this.user.moderator) return true
+      return this.hasAdminPermissions
+    },
+    hasPermissions: function () {
+      if (this.requires === 'admin') return this.hasAdminPermissions
+      if (this.requires === 'moderator') return this.hasModeratorPermissions
+      return this.userLogedIn
+    },
+  },
+  mounted () {
+    if (this.redirect !== '') this.$router.push(this.redirect)
+  },
+}
+</script>
+
+<style scoped>
+
+</style>
--- src/views/PageHome.vue
+++ src/views/PageHome.vue
@@ -1,12 +1,18 @@
 <template>
   <BasePage>
-
     <template slot="header">
       <h1>Hyped arena</h1>
     </template>
-
     <div class="home">
-      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+      <BaseAuth>
+        <p>Utilizador logado.</p>
+      </BaseAuth>
+      <BaseAuth requires="admin">
+        <p>Utilizador ADMIN.</p>
+      </BaseAuth>
+      <BaseAuth requires="moderator">
+        <p>Utilizador MODERADOR.</p>
+      </BaseAuth>
     </div>
 
   </BasePage>
